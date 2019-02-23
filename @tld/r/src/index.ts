aa7f76accfc67c179605a71519116244dc5b4cc1
miyaokamarina <miyaokamarina@gmail.com>
2019-02-23 18:38:20 +0300

    feat(r): Exposed `TldrContext`.

--- @tld/r/src/index.ts
+++ @tld/r/src/index.ts
@@ -3,6 +3,7 @@ export * from './React/string';
 export * from './React/time';
 export * from './React/tl';
 export * from './React/Tldr';
+export * from './React/TldrContext';
 export * from './React/translated';
 export * from './React/useCollator';
 export * from './React/useTlrd';
